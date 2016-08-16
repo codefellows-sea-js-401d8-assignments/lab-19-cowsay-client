292c0876b0d85843f2cca305533ede99b6cba61b
Gin <vienlyt@gmail.com>
2016-08-16 13:03:08 -0700

    finished

---
+++ lab-vien/app/entry.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// webpack assets
+require('!!file?name=[name].[ext]!./html/index.html');
+require('./style/scss/main.scss');
+
+// npm modules
+const angular = require('angular');
+const cowsay = require('cowsay-browser');
+// angular modules
+const app = angular.module('cowsayApp', []);
+// services
+// controllers
+app.controller('CowsayController', ['$scope', function($scope) {
+  $scope.cowtext = 'something';
+  $scope.cowType = 'default';
+  $scope.previousCow = '';
+  $scope.previousCowArray = [];
+
+  cowsay.list((err, list) => {
+    if (err) {
+      return $scope.allCowTypes = ['default'];
+    }
+    return $scope.allCowTypes = list;
+  });
+
+  $scope.say = function() {
+    return cowsay.say({
+      text: $scope.cowtext || '',
+      f: $scope.cowType,
+    });
+  };
+
+  $scope.save = function() {
+    $scope.previousCow = $scope.saved || '';
+    $scope.saved = $scope.say();
+  };
+
+  $scope.reset = function() {
+    $scope.saved = $scope.previousCow || '';
+  };
+}]);
+// components / directives
