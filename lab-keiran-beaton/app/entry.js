a6d9a7472e89c3831d6904b8bb881a02dd4fe0c8
keiran beaton <keiran beaton>
2016-08-16 11:01:07 -0700

    appears to be working

--- lab-keiran-beaton/app/entry.js
+++ lab-keiran-beaton/app/entry.js
@@ -9,13 +9,14 @@ const angular = require('angular');
 const cowsay = require('cowsay-browser');
 
 //angular module
-angular.module('demoApp', []);
+angular.module('cowsayApp', []);
 
 //controllers
-angular.module('demoApp').controller('CowsayController', ['$scope', function($scope){
-  $scope.cowText = 'wish i had something to say';
+angular.module('cowsayApp')
+.controller('CowsayController', ['$scope', function($scope){
+  $scope.cowText = 'Write your stuff here';
   $scope.say = function() {
-    return '\n' + cowsay.say({text: $scope.cowtext});
+    return '\n' + cowsay.say({text: $scope.cowText});
   };
   $scope.alreadySaid = function() {
     $scope.result = $scope.say();
